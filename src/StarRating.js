827a833be9cc251115ba1d9dc2b797671da72304
Martina <martina.gabrielli@yahoo.it>
2024-07-03 20:09:59 +0100

    add external use state and prop types

--- src/StarRating.js
+++ src/StarRating.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import PropTypes from 'prop-types';
 
 const containerStyle = {
     display: "flex",
@@ -10,14 +11,37 @@ const starContainerStyle = {
     display: "flex",
 }
 
-const textStyle = {
-    lineHeight: "1",
-    margin: "0",
+StarRating.propTypes = {
+    maxRating: PropTypes.number,
+    color: PropTypes.string,
+    size: PropTypes.number,
+    messages: PropTypes.array,
+    defaultRating: PropTypes.number,
+    onSetRating: PropTypes.func
 }
 
-export default function StarRating({maxRating = 5}) {
-    const [rating, setRating] = useState(0);
+export default function StarRating({
+    maxRating = 5,
+    color = '#fcc419',
+    size = 48,
+    messages = [],
+    defaultRating = 0,
+    onSetRating
+}) {
+    const [rating, setRating] = useState(defaultRating);
     const [tempRating, setTempRating] = useState(0);
+
+    const textStyle = {
+        lineHeight: "1",
+        margin: "0",
+        color,
+        size: `${size}px`
+    }
+
+    function handleMovieRating(rating) {
+        setRating(rating);
+        onSetRating(rating);
+    }
 
     return (
         <div style={containerStyle}>
@@ -26,25 +50,27 @@ export default function StarRating({maxRating = 5}) {
                     <Star 
                         key={1} 
                         full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-                        onClick={() => setRating(i + 1)} 
+                        onClick={() => handleMovieRating(i + 1)} 
                         onMouseEnter={() => setTempRating(i + 1)} 
-                        onMouseLeave={() => setTempRating(0)} 
+                        onMouseLeave={() => setTempRating(0)}
+                        color={color}
+                        size={size} 
                     />
                 ))}
             </div>
-            <p style={textStyle}>{tempRating || rating || ""}</p>
+            <p style={textStyle}>{messages.length === maxRating ? messages[tempRating ? tempRating - 1 : rating - 1] : tempRating || rating || ""}</p>
         </div>
     )
 }
 
-const starStyle = {
-    width: "48px",
-    height: "48px",
-    display: "block",
-    cursor: "pointer",
-};
+function Star({onClick, onMouseEnter, onMouseLeave, full, color, size}) {
+    const starStyle = {
+        width: `${size}px`,
+        height: `${size}px`,
+        display: "block",
+        cursor: "pointer",
+    };
 
-function Star({onClick, onMouseEnter, onMouseLeave, full}) {
     return (
         <span 
             onClick={onClick} 
@@ -57,8 +83,8 @@ function Star({onClick, onMouseEnter, onMouseLeave, full}) {
             <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 20 20"
-            fill="#000"
-            stroke="#000"
+            fill={color}
+            stroke={color}
             >
             <path
                 d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"
@@ -69,7 +95,7 @@ function Star({onClick, onMouseEnter, onMouseLeave, full}) {
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
-            stroke="#000"
+            stroke={color}
             >
             <path
                 strokeLinecap="round"
--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 import StarRating from './StarRating';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating onSetRating={setMovieRating}/>
+      The rating is {movieRating}
+    </div>
+  )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating maxRating={5} />
+    <StarRating maxRating={5} messages={['Terrible', 'Bad', 'Okay', 'Great', 'Amazing']} defaultRating={3} />
+    <Test/>
   </React.StrictMode>
 );
