b5f0cf0fcd503d9b6cb800f80a32e5f762f8f89b
martinagabrielli <martina.gabrielli@yahoo.it>
2024-08-31 19:21:00 +0100

    update document title

--- src/App.js
+++ src/App.js
@@ -315,6 +315,15 @@ function MovieDetails({selectedId, onCloseMovie, onAddWatched, watched}) {
     getMovieDetails();
   }, [selectedId]);
 
+  useEffect(function() {
+    if (!title) return;
+    document.title = `Movie | ${title}`;
+
+    return function() {
+      document.title = 'usePopcorn';
+    }
+  }, [title]);
+
   return <div className="details">
     {isLoading ? <Loader/> :
       <>
